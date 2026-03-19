ca1f67ebb8ee0393f42634f14a80abca68f5347e
guptakaran0720 <guptakaran0720@gmail.com>
2026-03-19 22:30:20 +0530

    fix oauth

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ['profile', 'email'],
     },
     async (accessToken, refreshToken, profile, done) => {
